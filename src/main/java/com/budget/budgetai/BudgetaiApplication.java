9c4968cb561f3994ad01c71ea10cf82b68772fcf
Gabe <dejonghgabe1@gmail.com>
2026-02-26 08:17:14 -0500

    Initial commit

---
+++ src/main/java/com/budget/budgetai/BudgetaiApplication.java
@@ -0,0 +1,13 @@
+package com.budget.budgetai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/budget/budgetai/BudgetaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budget.budgetai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
